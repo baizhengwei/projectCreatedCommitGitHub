6cc94e5f62fb76be9cb14c4985b349d5abe59629
bzw <bai_zhengwei@foxmail.com>
2020-08-28 14:15:55 +0800

    tag1

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <a>asfdsafssfsda</a>
     </div>
     <router-view/>
   </div>
